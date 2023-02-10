ab05fd3fd6e50e38bfaeb4309dcc6707b0f5562f
Kovacs Jacint <jacint1998@gmail.com>
2023-02-10 10:08:24 +0200

    css files added to index.js and fixed

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/animals.css'
+import './styles/boarding.css'
+import './styles/doctor.css'
+import './styles/ellipse.css'
+import './styles/footer.css'
+import './styles/hero.css'
+import './styles/menu.css'
+import './styles/paws.css'
+import './styles/shelter.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
